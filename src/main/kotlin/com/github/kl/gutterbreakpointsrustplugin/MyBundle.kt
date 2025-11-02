b41699c2f0f0f4d972f39c22e9cf075ecfb2e19d
GitHub Action <action@github.com>
2025-11-02 17:29:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/kl/gutterbreakpointsrustplugin/MyBundle.kt
+++ src/main/kotlin/com/github/kl/gutterbreakpointsrustplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kl.gutterbreakpointsrustplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kl/gutterbreakpointsrustplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/kl/gutterbreakpointsrustplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.kl.gutterbreakpointsrustplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.kl.gutterbreakpointsrustplugin.MyBundle
+import com.github.kl.gutterbreakpointsrustplugin.services.MyProjectService
 import javax.swing.JButton
 
 
